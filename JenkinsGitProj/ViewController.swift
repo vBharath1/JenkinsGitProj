7ed1a89d85c1d53d49de80b152ad759b6ca6fef2
Bharath Vanapala <bvanapala@juniper.net>
2018-11-29 11:31:24 +0530

    proj commit

---
+++ JenkinsGitProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsGitProj
+//
+//  Created by Bharath Vanapala on 29/11/18.
+//  Copyright © 2018 Bharath Vanapala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
